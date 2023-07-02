60bc6792754160de121c2a9a059cae4bc5265e82
SepehrImanian <s.imanian@tapsell.ir>
2023-07-02 10:51:37 +0330

    Add Quota metrics, Add flag golang metrics for disable or enable default golang metrics, Refactor codes

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	log "github.com/sirupsen/logrus"
 )
@@ -23,19 +23,31 @@ const (
 )
 
 type S3Collector struct {
-	sizeActualMetric      *prometheus.Desc
-	totalSizeActualMetric *prometheus.Desc
-	sizeUtilizedMetric    *prometheus.Desc
-	numObjectsMetric      *prometheus.Desc
-	cephAccessKey         string
-	cephSecretKey         string
-	cephGatewayURL        string
-	bucketStats           map[string]BucketStats
+	sizeActualMetric           *prometheus.Desc
+	totalUsageSizeActualMetric *prometheus.Desc
+	sizeUtilizedMetric         *prometheus.Desc
+	numObjectsMetric           *prometheus.Desc
+	perUserUsageMetric         *prometheus.Desc
+	bucketQuotaMaxSize         *prometheus.Desc
+	bucketQuotaMaxObjects      *prometheus.Desc
+	bucketNumShards            *prometheus.Desc
+	cephAccessKey              string
+	cephSecretKey              string
+	cephGatewayURL             string
+	bucketStats                map[string]BucketStats
 }
 
 type BucketStats struct {
-	Name  string       `json:"bucket"`
-	Usage UsageDetails `json:"usage"`
+	Name        string             `json:"bucket"`
+	Usage       UsageDetails       `json:"usage"`
+	BucketQuota BucketQuotaDetails `json:"bucket_quota"`
+	BucketOwner string             `json:"owner"`
+	NumShards   int                `json:"num_shards"`
+}
+
+type BucketQuotaDetails struct {
+	MaxSize    int `json:"max_size"`
+	MaxObjects int `json:"max_objects"`
 }
 
 type UsageDetails struct {
@@ -90,15 +102,65 @@ func (collector *S3Collector) updateBucketStats() error {
 	return nil
 }
 
+func CalculateBucketsTotalSizeMetric(collector *S3Collector, ch chan<- prometheus.Metric) {
+	// // Calculate total size across all buckets
+	var totalSize float64
+	for _, stats := range collector.bucketStats {
+		totalSize += float64(stats.Usage.SizeDetails.SizeActual)
+	}
+
+	ch <- prometheus.MustNewConstMetric(
+		collector.totalUsageSizeActualMetric,
+		prometheus.GaugeValue,
+		totalSize,
+	)
+}
+
+func CalculateBucketsSizesMetrics(collector *S3Collector, ch chan<- prometheus.Metric) {
+	// Collect the metrics
+	for bucketName, stats := range collector.bucketStats {
+		ch <- prometheus.MustNewConstMetric(collector.sizeActualMetric, prometheus.GaugeValue, float64(stats.Usage.SizeDetails.SizeActual), bucketName)
+		ch <- prometheus.MustNewConstMetric(collector.sizeUtilizedMetric, prometheus.GaugeValue, float64(stats.Usage.SizeDetails.SizeUtilized), bucketName)
+		ch <- prometheus.MustNewConstMetric(collector.numObjectsMetric, prometheus.GaugeValue, float64(stats.Usage.SizeDetails.NumObjects), bucketName)
+		ch <- prometheus.MustNewConstMetric(collector.bucketNumShards, prometheus.GaugeValue, float64(stats.NumShards), bucketName)
+	}
+}
+
+func PerUserUsageMetrics(collector *S3Collector, ch chan<- prometheus.Metric) {
+	uniqueOwners := make(map[string]*float64, len(collector.bucketStats))
+
+	for _, stats := range collector.bucketStats {
+		owner := stats.BucketOwner
+		totalSizePtr, exists := uniqueOwners[owner]
+		if !exists {
+			totalSize := float64(0)
+			totalSizePtr = &totalSize
+			uniqueOwners[owner] = totalSizePtr
+		}
+		*totalSizePtr += float64(stats.Usage.SizeDetails.SizeActual)
+	}
+
+	for owner, totalSizePtr := range uniqueOwners {
+		ch <- prometheus.MustNewConstMetric(collector.perUserUsageMetric, prometheus.GaugeValue, *totalSizePtr, owner)
+	}
+}
+
+func ExposeBucketQuotaMetrics(collector *S3Collector, ch chan<- prometheus.Metric) {
+	for bucketName, stats := range collector.bucketStats {
+		ch <- prometheus.MustNewConstMetric(collector.bucketQuotaMaxSize, prometheus.GaugeValue, float64(stats.BucketQuota.MaxSize), bucketName)
+		ch <- prometheus.MustNewConstMetric(collector.bucketQuotaMaxObjects, prometheus.GaugeValue, float64(stats.BucketQuota.MaxObjects), bucketName)
+	}
+}
+
 func newS3Collector(cephAccessKey, cephSecretKey, cephGatewayURL string) *S3Collector {
 	return &S3Collector{
 		sizeActualMetric: prometheus.NewDesc("s3_bucket_actual_size",
 			"s3 bucket size",
 			[]string{"name"}, nil,
 		),
-		totalSizeActualMetric: prometheus.NewDesc("s3_bucket_total_size",
-			"s3 total buckets size",
-			[]string{"category"}, nil,
+		totalUsageSizeActualMetric: prometheus.NewDesc("s3_bucket_total_usage_size",
+			"s3 total usage all buckets size",
+			nil, nil,
 		),
 		sizeUtilizedMetric: prometheus.NewDesc("s3_bucket_utilized_size",
 			"s3 bucket utilized size",
@@ -108,6 +170,22 @@ func newS3Collector(cephAccessKey, cephSecretKey, cephGatewayURL string) *S3Coll
 			"s3 bucket number of objects",
 			[]string{"name"}, nil,
 		),
+		perUserUsageMetric: prometheus.NewDesc("s3_bucket_per_user_usage_size",
+			"size of each user",
+			[]string{"name"}, nil,
+		),
+		bucketQuotaMaxSize: prometheus.NewDesc("s3_bucket_quota_max_size",
+			"bucket quota max size",
+			[]string{"name"}, nil,
+		),
+		bucketQuotaMaxObjects: prometheus.NewDesc("s3_bucket_quota_max_objects",
+			"bucket quota max objects",
+			[]string{"name"}, nil,
+		),
+		bucketNumShards: prometheus.NewDesc("s3_bucket_num_shards",
+			"bucket number of shards",
+			[]string{"name"}, nil,
+		),
 		cephAccessKey:  cephAccessKey,
 		cephSecretKey:  cephSecretKey,
 		cephGatewayURL: cephGatewayURL,
@@ -115,11 +193,22 @@ func newS3Collector(cephAccessKey, cephSecretKey, cephGatewayURL string) *S3Coll
 	}
 }
 
+func getEnv(key string, defaultVal string) string {
+	if env, ok := os.LookupEnv(key); ok {
+		return env
+	}
+	return defaultVal
+}
+
 func (collector *S3Collector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.sizeActualMetric
-	ch <- collector.totalSizeActualMetric
+	ch <- collector.totalUsageSizeActualMetric
 	ch <- collector.numObjectsMetric
 	ch <- collector.numObjectsMetric
+	ch <- collector.perUserUsageMetric
+	ch <- collector.bucketQuotaMaxObjects
+	ch <- collector.bucketQuotaMaxSize
+	ch <- collector.bucketNumShards
 }
 
 func (collector *S3Collector) Collect(ch chan<- prometheus.Metric) {
@@ -130,41 +219,10 @@ func (collector *S3Collector) Collect(ch chan<- prometheus.Metric) {
 		return
 	}
 
-	// Collect the metrics
-	for bucketName, stats := range collector.bucketStats {
-		ch <- prometheus.MustNewConstMetric(collector.sizeActualMetric, prometheus.GaugeValue, float64(stats.Usage.SizeDetails.SizeActual), bucketName)
-		ch <- prometheus.MustNewConstMetric(collector.sizeUtilizedMetric, prometheus.GaugeValue, float64(stats.Usage.SizeDetails.SizeUtilized), bucketName)
-		ch <- prometheus.MustNewConstMetric(collector.numObjectsMetric, prometheus.GaugeValue, float64(stats.Usage.SizeDetails.NumObjects), bucketName)
-	}
-
-	// // Calculate total size across all buckets
-	var totalSize float64
-	for _, stats := range collector.bucketStats {
-		totalSize += float64(stats.Usage.SizeDetails.SizeActual)
-	}
-
-	sizes := map[string]float64{
-		"KB": totalSize,
-		"MB": totalSize / 1024,
-		"GB": totalSize / (1024 * 1024),
-		"TB": totalSize / (1024 * 1024 * 1024),
-	}
-
-	for unit, size := range sizes {
-		ch <- prometheus.MustNewConstMetric(
-			collector.totalSizeActualMetric,
-			prometheus.GaugeValue,
-			size,
-			unit,
-		)
-	}
-}
-
-func getEnv(key string, defaultVal string) string {
-	if env, ok := os.LookupEnv(key); ok {
-		return env
-	}
-	return defaultVal
+	PerUserUsageMetrics(collector, ch)
+	CalculateBucketsSizesMetrics(collector, ch)
+	CalculateBucketsTotalSizeMetric(collector, ch)
+	ExposeBucketQuotaMetrics(collector, ch)
 }
 
 func init() {
@@ -176,6 +234,7 @@ func main() {
 		printVersion      = flag.Bool("version", false, "Print version information.")
 		listenAddress     = flag.String("web.listen-address", getEnv("LISTEN_ADDRESS", ":9290"), "Address to listen on for web interface and telemetry.")
 		metricsPath       = flag.String("web.telemetry-path", getEnv("METRIC_PATH", "/metrics"), "Path under which to expose metrics.")
+		GolangMetrics     = flag.Bool("web.golang-metrics", false, "Disable/enable default golang metrics 'true/false'")
 		cephRadosgwURI    = flag.String("radosgw.server", getEnv("CEPH_URL", "http://localhost:9000"), "HTTP address of the ceph radosgw server")
 		cephRadosgwKey    = flag.String("radosgw.access-key", getEnv("CEPH_ACCESS_KEY", ""), "The access key used to login in to ceph radosgw.")
 		cephRadosgwSecret = flag.String("radosgw.access-secret", getEnv("CEPH_ACCESS_SECRET", ""), "The access secret used to login in to ceph radosgw")
@@ -192,9 +251,28 @@ func main() {
 	log.Infoln("Build context", version.BuildContext())
 
 	s3 := newS3Collector(*cephRadosgwKey, *cephRadosgwSecret, *cephRadosgwURI)
-	prometheus.MustRegister(s3)
 
-	http.Handle(*metricsPath, promhttp.Handler())
+	promReg := prometheus.NewRegistry()
+
+	prometheus.MustRegister(s3)
+	promReg.MustRegister(s3)
+
+	var handler http.Handler
+
+	if !*GolangMetrics {
+		handler = promhttp.HandlerFor(
+			promReg,
+			promhttp.HandlerOpts{
+				EnableOpenMetrics: *GolangMetrics,
+			},
+		)
+	} else {
+		handler = promhttp.Handler()
+	}
+
+	fmt.Println("GolangMetrics", *GolangMetrics)
+
+	http.Handle(*metricsPath, handler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
                         <head><title>s3-ceph Exporter</title></head>
